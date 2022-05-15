6ee6a72ef154e23f03e764825d7d08add0c1cddf
adnaneaabbar <adnaneaabbar@gmail.com>
2022-05-15 22:57:20 +0100

    Reverse a linked list

---
+++ week3/reverse_linked_list.py
@@ -0,0 +1,79 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+'''
+Sample Input
+1
+5
+1
+2
+3
+4
+5
+
+Sample Output
+5 4 3 2 1
+
+# Complete the 'reverse' function below.
+#
+# The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+# The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+#
+'''
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+def reverse(llist):
+    # Write your code here
+    nxt = None
+    while llist:
+        tmp = llist.next
+        llist.next = nxt
+        nxt = llist
+        llist = tmp
+    
+    return nxt
+    
+if __name__ == '__main__':
